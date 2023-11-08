afcd9daf0d5d61f6ad227d1ef9a2e7dc6894cbb5
EduardYan <eduarygp@gmail.com>
2023-11-08 12:15:25 -0600

    First commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000;
+
+export const DB_HOST = process.env.DB_HOST || "monorail.proxy.rlwy.net";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "4de22bb5C4dB24fCC11g1gA15f64ha4e";
+export const DB_NAME = process.env.DB_NAME || "usersdb";
+export const DB_PORT = process.env.DB_PORT || 32997;
